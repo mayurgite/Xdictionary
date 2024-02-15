7dc5e19cf60a75e759a2cd28c0f2425197fa65a9
Mayur Gite <mayurgite.it@gmail.com>
2024-02-15 17:36:34 +0530

    test

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -23,7 +23,7 @@ const XDictionary = () => {
 
     return (
         <div>
-            <h1>XDictionary</h1>
+            <h1>Dictionary App</h1>
             <input
                 type="text"
                 placeholder="Search for a word..."
